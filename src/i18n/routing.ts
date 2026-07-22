c774a74198608f85d8b24a158df271f9691fb4d3
Marcos Gonçalves <61754489+zArkade@users.noreply.github.com>
2026-07-22 20:44:49 -0300

    Fix: deu errado T_T

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["pt-BR", "en-US"],
   defaultLocale: "pt-BR",
-  localePrefix: "as-needed",
+  localePrefix: "always",
   localeDetection: false,
 });
