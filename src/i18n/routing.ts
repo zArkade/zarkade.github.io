632dcb4c6882071cd2fb97c783389d3b84a215b4
Marcos Gonçalves <61754489+zArkade@users.noreply.github.com>
2026-07-22 20:41:31 -0300

    Fix: ajuste de rota por conta de generateStaticParams

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["pt-BR", "en-US"],
   defaultLocale: "pt-BR",
-  localePrefix: "always",
+  localePrefix: "as-needed",
   localeDetection: false,
 });
